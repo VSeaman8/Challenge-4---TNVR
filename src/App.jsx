151546a717dd39d5cc2feec8f4090806393e1fd2
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-02-22 08:36:39 +0000

    Intial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import MyComponent from "./components/MyComponent";
+
 const App = () => {
-    return <></>;
+  return (
+    <>
+      <Router></Router>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <h1>just checking it workd</h1>;
+};
+
+export default MyComponent;
