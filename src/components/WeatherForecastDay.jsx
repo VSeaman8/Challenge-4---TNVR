7632f1859ada14e065668030502208a3bedf8f30
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-02-27 17:28:12 +0000

    UPDATED weather icons now working

--- src/components/WeatherForecastDay.jsx
+++ src/components/WeatherForecastDay.jsx
@@ -2,7 +2,7 @@ const WeatherForecastDay = ({ day, temperature, weather, icon }) => {
   return (
     <div className="weather-day">
       <h2>{day}</h2>
-      <div className="weather-icon">{icon}</div>
+      <img src={`/assets/weather-icons/${icon}.svg`} className="weather-icon" />
       <p>{temperature}</p>
       <p>{weather}</p>
     </div>
